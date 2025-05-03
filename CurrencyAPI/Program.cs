8bb93a4ac392f81344cfac6260866e5fbcb6fa0c
Varun Teja <35555010+VarunSaiTeja@users.noreply.github.com>
2025-05-03 15:47:35 +0530

    added Polly for Resilience

--- CurrencyAPI/Program.cs
+++ CurrencyAPI/Program.cs
@@ -1,3 +1,4 @@
+using CurrencyAPI.Extensions;
 using CurrencyAPI.Providers;
 using CurrencyAPI.Providers.Frankfurter;
 using FluentValidation;
@@ -14,7 +15,9 @@ builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services
     .AddRefitClient<IFrankfurterApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api.frankfurter.dev/v1"));
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api.frankfurter.dev/v1"))
+    .AddPolicyHandler(PollyExtensions.GetRetryPolicy())
+    .AddPolicyHandler(PollyExtensions.GetCircuitBreakerPolicy());
 
 builder.Services.AddKeyedScoped<ICurrencyProvider, FrankfurterProvider>(CurrencyProviderType.Frankfurter);
 builder.Services.AddScoped<CurrencyProviderFactory>();
